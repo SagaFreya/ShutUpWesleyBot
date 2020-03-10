b70684b9fee1ae8ac04a9438d90e424870a18d8f
SagaFreya <ssw663@gmail.com>
2020-03-10 21:56:32 +0000

    Fetch Wesley's latest comments

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import praw
 reddit = praw.Reddit('bot1', user_agent='linux:ml.sagafreya.shutupwesleybot:v0.0.1 (by /u/ssw663)')
 
 print(reddit.read_only)
+
+wes = reddit.redditor('Wesley_Ford')
+
+for c in wes.stream.comments():
+    print(c.body)
