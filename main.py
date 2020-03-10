6eb17b18e02edfd4e49a1aa80ec0f8ba4d676645
SagaFreya <ssw663@gmail.com>
2020-03-10 22:55:27 +0000

    Store already seen comments to avoid duplicates

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import praw
 
-reddit = praw.Reddit('bot1', user_agent='linux:ml.sagafreya.shutupwesleybot:v0.0.1 (by /u/ssw663)')
+reddit = praw.Reddit('bot1', user_agent='linux:ml.sagafreya.shutupwesleybot:v0.0.2 (by /u/ssw663)')
 
 print(reddit.read_only)
+
+f = open('commented.txt', 'r+')
+
+commented = f.read().splitlines()
 
 wes = reddit.redditor('Wesley_Ford')
 
 for c in wes.stream.comments():
-    print(c.body)
+    if c.id not in commented:
+        print(c.body)
+        f.write(c.id + '\n')
