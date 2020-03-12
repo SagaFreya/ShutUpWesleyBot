885ecd3aeb7d335e98b9ad548ff2da8b2c8fd712
SagaFreya <ssw663@gmail.com>
2020-03-12 21:38:12 +0000

    Remove multithreading because of bugs

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-from time import sleep
-import threading
-
 import praw
 
 reddit = praw.Reddit('bot1', user_agent='linux:ml.sagafreya.shutupwesleybot:v0.1.0 (by /u/ssw663)')
-
 print(reddit.read_only)
 
 f = open('commented.txt', 'r+')
-
+f.seek(0)
 commented = f.read().splitlines()
 
 wes = reddit.redditor('Wesley_Ford')
-
 bot = reddit.redditor('ShutUpWesleyBot')
 
 def delete_comments():
-    while True:
-        sleep(10)
-        for c in bot.comments.new(limit=20):
-            if c.score < 0:
-                print('Deleted: ' + c.body)
-                c.delete()
+    for c in bot.comments.new(limit=10):
+        if c.score < 0:
+            print('Deleted: ' + c.body)
+            c.delete()
 
-t = threading.Thread(target=delete_comments)
-
-t.start()
+delete_comments()
 
 for c in wes.stream.comments():
     if c.id not in commented:
-        print(c.body)
+        print('Found: ' + c.body)
         f.write(c.id + '\n')
         c.reply('Shut up, Wesley.')
+        delete_comments()
 
-t.join()
 f.close()
