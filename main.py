198e47d53b7b21ee58252d0cdae81e1fe024a64a
SagaFreya <ssw663@gmail.com>
2020-03-12 14:45:34 +0000

    Delete downvoted comments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from time import sleep
+import threading
+
 import praw
 
 reddit = praw.Reddit('bot1', user_agent='linux:ml.sagafreya.shutupwesleybot:v0.1.0 (by /u/ssw663)')
@@ -10,8 +13,25 @@ commented = f.read().splitlines()
 
 wes = reddit.redditor('Wesley_Ford')
 
+bot = reddit.redditor('ShutUpWesleyBot')
+
+def delete_comments():
+    while True:
+        sleep(10)
+        for c in bot.comments.new(limit=20):
+            if c.score < 0:
+                print('Deleted: ' + c.body)
+                c.delete()
+
+t = threading.Thread(target=delete_comments)
+
+t.start()
+
 for c in wes.stream.comments():
     if c.id not in commented:
         print(c.body)
         f.write(c.id + '\n')
         c.reply('Shut up, Wesley.')
+
+t.join()
+f.close()
